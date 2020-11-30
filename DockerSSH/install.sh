78b47af21fa7fd846f20d7842b35f0c895a99b7c
DeedWark <deedwark@hotmail.fr>
2020-11-30 17:51:45 +0100

    Ask pass / name

--- DockerSSH/install.sh
+++ DockerSSH/install.sh
@@ -5,19 +5,23 @@ if [ $UID -ne 0 ] ; then
 	exit 1
 fi
 
-read -r -p "Password for SSH: " sshpwd
-echo -e "Do not forget to change for production"
+read -r -p "Password for SSH: " -s sshpwd
+echo -e "\n!!! Do not forget to change password for production !!!"
 
-sed -ri 's/dockerSSH/${sshpwd}/g' ./Dockerfile
+sed -ri "s/dockerSSH/${sshpwd}/g" ./Dockerfile
+
+read -r -p "Container name: " lxname
 
 docker build -t secureshell .
 
-docker create -it --name="secureshell" --hostname="ssh" --restart="always" secureshell
+docker create -it --name="${lxname}" --hostname="ssh" --restart="always" secureshell
 
-docker start secureshell 2>/dev/null
+docker start ${lxname} 2>/dev/null
 
-sshdocker=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" secureshell 2>/dev/null)
+sshdocker=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" ${lxname} 2>/dev/null)
 
-echo -e "secureshell IP: ${sshdocker}"
+echo -e "${lxname} IP: ${sshdocker}"
 
-echo -e "To enter in secureshell docker\nssh root@${sshdocker}"
+echo -e "To enter in ${lxname} docker\nssh root@${sshdocker}"
+
+sed -ri "s/${sshpwd}/dockerSSH/g" ./Dockerfile
